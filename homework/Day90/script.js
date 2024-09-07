b0cd66e9fbe6392508ddb26116310450130a8747
nikobucxjunior <nikolozbucx3@gmail.com>
2024-09-07 12:59:52 -0400

    .

---
+++ homework/Day90/script.js
@@ -0,0 +1,13 @@
+let person = {
+    firstName: "Nikusha",
+    lastName: "Butskhrikidze",
+    age: 14,
+    gender: "Male",
+    address: {
+        street: "18 street",
+        city: "kutaisi",
+        country: "Georgia"
+    }
+};
+
+console.log(person);
---
+++ homework/Day91/script.js
@@ -0,0 +1,6 @@
+function getMaxValue(...numbers) {
+    return Math.max(...numbers);
+}
+
+// Example
+console.log(getMaxValue(3, 9, 12, 5, 8)); // Output: 12
